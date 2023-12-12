ed4ef6f8826d70f87a6bb8dcb6b9cbe656cec80e
Richard <richardvu93@gmail.com>
2023-12-12 14:43:32 -0800

    gallery update

--- src/pages/Gallery.jsx
+++ src/pages/Gallery.jsx
@@ -1,4 +1,16 @@
+import { Outlet } from "react-router-dom";
+import GalleryPage1 from "../components/GalleryPage1";
+
 function Gallery() {
-  return <div>Gallery</div>;
+  return (
+    <div className=''>
+      <div className='gallery-title'>
+        <h1 className='gallery-title__text'>Gallery</h1>
+      </div>
+
+      <Outlet />
+      {/* <GalleryPage1 /> */}
+    </div>
+  );
 }
 export default Gallery;
