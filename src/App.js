5f175f935698e0e3026eeac3a5af740c5daf8886
Richard <richardvu93@gmail.com>
2023-12-12 14:39:21 -0800

    nesting gallery route

--- src/App.js
+++ src/App.js
@@ -8,6 +8,9 @@ import Blog from "./pages/Blog";
 import Schedule from "./pages/Schedule";
 import Gallery from "./pages/Gallery";
 import Contact from "./pages/Contact";
+import Footer from "./components/Footer";
+import GalleryPage1 from "./components/GalleryPage1";
+import GalleryPage2 from "./components/GalleryPage2";
 
 function App() {
   return (
@@ -16,7 +19,15 @@ function App() {
       <Routes>
         <Route path='/' element={<Home />} />
         <Route path='about' element={<About />} />
-        <Route path='gallery' element={<Gallery />} />
+        <Route path='gallery' element={<Gallery />}>
+          {/* <Route index element={<GalleryPage1 />} /> */}
+          <Route index element={<GalleryPage1 />} />
+          <Route path='page-1' element={<GalleryPage1 />} />
+          <Route path='page-2' element={<GalleryPage2 />} />
+        </Route>
+        {/* <Route path='gallery/page-1' element={<GalleryPage1 />} /> */}
+        {/* <Route path='gallery/page-2' element={<GalleryPage2 />} /> */}
+
         <Route path='schedule' element={<Schedule />} />
         <Route path='blog' element={<Blog />} />
         <Route path='pricing' element={<Pricing />} />
@@ -24,6 +35,7 @@ function App() {
         <Route path='contact' element={<Contact />} />
         {/* <Route path='*' element={<NotFound />} /> */}
       </Routes>
+      <Footer />
     </>
   );
 }
