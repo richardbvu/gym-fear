dbee6f76d7a3ef8bc330a6022a9af3e9b3e94cea
Richard <richardvu93@gmail.com>
2023-12-14 18:08:20 -0800

    added new images/background/title/time

--- src/components/GalleryFiles.js
+++ src/components/GalleryFiles.js
@@ -7,17 +7,78 @@ import gallery6 from "../images/gallery6.jpg";
 import gallery7 from "../images/gallery7.jpg";
 import gallery8 from "../images/gallery8.jpg";
 import gallery9 from "../images/gallery9.jpg";
-import gallery10 from "../images/gallery10.jpg";
+import gallery1_small from "../images/gallery1-small.jpg";
+import gallery2_small from "../images/gallery2-small.jpg";
+import gallery3_small from "../images/gallery3-small.jpg";
+import gallery4_small from "../images/gallery4-small.jpg";
+import gallery5_small from "../images/gallery5-small.jpg";
+import gallery6_small from "../images/gallery6-small.jpg";
+import gallery7_small from "../images/gallery7-small.jpg";
+import gallery8_small from "../images/gallery8-small.jpg";
+import gallery9_small from "../images/gallery9-small.jpg";
 
 export const galleryImg = [
-  { img: gallery1, id: 1 },
-  { img: gallery2, id: 2 },
-  { img: gallery3, id: 3 },
-  { img: gallery4, id: 4 },
-  { img: gallery5, id: 5 },
-  { img: gallery6, id: 6 },
-  { img: gallery7, id: 7 },
-  { img: gallery8, id: 8 },
-  { img: gallery9, id: 9 },
-  { img: gallery10, id: 10 },
+  {
+    img: gallery1,
+    background: gallery1_small,
+    id: "1",
+    title: "Aerobics",
+    time: "Mon-Fri",
+  },
+  {
+    img: gallery2,
+    background: gallery2_small,
+    id: 2,
+    title: "Power Lifting",
+    time: "Wed-Fri",
+  },
+  {
+    img: gallery3,
+    background: gallery3_small,
+    id: 3,
+    title: "Senior Fitness",
+    time: "Wed-Thu",
+  },
+  {
+    img: gallery4,
+    background: gallery4_small,
+    id: 4,
+    title: "Boxing",
+    time: "Mon-Wed",
+  },
+  {
+    img: gallery5,
+    background: gallery5_small,
+    id: 5,
+    title: "Strength",
+    time: "Sat-Sun",
+  },
+  {
+    img: gallery6,
+    background: gallery6_small,
+    id: 6,
+    title: "Yoga",
+    time: "Mon-Fri",
+  },
+  {
+    img: gallery7,
+    background: gallery7_small,
+    id: 7,
+    title: "Body Fear",
+    time: "Thu-Fri",
+  },
+  {
+    img: gallery8,
+    background: gallery8_small,
+    id: 8,
+    title: "Fear Fit",
+    time: "Sat-Sun",
+  },
+  {
+    img: gallery9,
+    background: gallery9_small,
+    id: 9,
+    title: "Zumbey",
+    time: "Mon-Fri",
+  },
 ];
