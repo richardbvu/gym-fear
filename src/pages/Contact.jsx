8071c625941864398b9194cc82b436951f104665
Richard <richardvu93@gmail.com>
2023-12-16 16:10:38 -0800

    title and component

--- src/pages/Contact.jsx
+++ src/pages/Contact.jsx
@@ -1,4 +1,13 @@
+import ContactUs from "../components/ContactUs";
+
 function Contact() {
-  return <div>Contact</div>;
+  return (
+    <div>
+      <div className='contact-title'>
+        <h1 className='contact-title__text'>Contact</h1>
+      </div>
+      <ContactUs />
+    </div>
+  );
 }
 export default Contact;
