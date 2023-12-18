3f92a87d33c8977239fc243e3662cafc074737b6
Richard <richardvu93@gmail.com>
2023-12-18 12:57:52 -0800

    Initial commit

---
+++ src/pages/MyAccount.jsx
@@ -0,0 +1,10 @@
+const MyAccount = () => {
+  return (
+    <div>
+      <div className='page-title'>
+        <h1 className='page-title__text'>Sign Up</h1>
+      </div>
+    </div>
+  );
+};
+export default MyAccount;
