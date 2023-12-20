dca9a7aacfeb6c7634cf0576fd7ba1ff662d4f22
Richard <richardvu93@gmail.com>
2023-12-19 17:53:18 -0800

    responsive update

--- src/components/Nav.jsx
+++ src/components/Nav.jsx
@@ -1,50 +1,77 @@
 import { Link } from "react-router-dom";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
-import { faUser } from "@fortawesome/free-solid-svg-icons";
-import { faSquarePlus } from "@fortawesome/free-solid-svg-icons";
+import {
+  faUser,
+  faSquarePlus,
+  faBars,
+  faXmark,
+} from "@fortawesome/free-solid-svg-icons";
+import { useRef } from "react";
 
 const Nav = () => {
+  const navRefLinks = useRef();
+  const navRefButtons = useRef();
+
+  const showNavBar = () => {
+    navRefLinks.current.classList.toggle("active");
+    navRefButtons.current.classList.toggle("active");
+  };
+
+  const noShowNavBar = () => {
+    navRefLinks.current.classList.toggle("active");
+    navRefButtons.current.classList.toggle("active");
+  };
   return (
-    <nav className='nav aqua'>
+    <nav className='nav'>
       <div className='nav__container'>
         <div className='nav__logo'>
           <div>
             <a href='/'>GYM FEAR</a>
           </div>
         </div>
-        <div className='nav__links'>
-          <Link className='nav__link' to='/'>
+        <div ref={navRefLinks} className='nav__links'>
+          <Link className='nav__link' to='/' onClick={noShowNavBar}>
             Home
           </Link>
-          <Link className='nav__link' to='about'>
+          <Link className='nav__link' to='about' onClick={noShowNavBar}>
             About
           </Link>
-          <Link className='nav__link' to='classes'>
+          <Link className='nav__link' to='classes' onClick={noShowNavBar}>
             Classes
           </Link>
-          <Link className='nav__link' to='schedule'>
+          <Link className='nav__link' to='schedule' onClick={noShowNavBar}>
             Schedule
           </Link>
-          <Link className='nav__link' to='pricing'>
+          <Link className='nav__link' to='pricing' onClick={noShowNavBar}>
             Pricing
           </Link>
-
-          <Link className='nav__link' to='gallery'>
+          <Link className='nav__link' to='gallery' onClick={noShowNavBar}>
             Gallery
           </Link>
-          <Link className='nav__link' to='contact'>
+          <Link className='nav__link' to='contact' onClick={noShowNavBar}>
             Contact
           </Link>
         </div>
-        <div className='nav__buttons'>
-          <Link className='nav__button-link' to='/account'>
+        <div ref={navRefButtons} className='nav__buttons'>
+          <Link
+            className='nav__button-link'
+            to='/account'
+            onClick={noShowNavBar}
+          >
             <FontAwesomeIcon icon={faUser} />
             My Account
           </Link>
-          <Link className='nav__button-link' to='/contact'>
+          <Link
+            className='nav__button-link'
+            to='/contact'
+            onClick={noShowNavBar}
+          >
             <FontAwesomeIcon icon={faSquarePlus} />
             Join Us
           </Link>
+        </div>
+        <div className='nav__toggle'>
+          <FontAwesomeIcon icon={faBars} id='nav-icon' onClick={showNavBar} />
         </div>
       </div>
     </nav>
