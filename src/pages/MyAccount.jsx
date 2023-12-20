fb6cd15193f736518ce97e764146e1e1feb5bae2
Richard <richardvu93@gmail.com>
2023-12-19 17:54:26 -0800

    account update

--- src/pages/MyAccount.jsx
+++ src/pages/MyAccount.jsx
@@ -4,6 +4,38 @@ const MyAccount = () => {
       <div className='page-title'>
         <h1 className='page-title__text'>Sign Up</h1>
       </div>
+      <div className='my-account'>
+        <div className='my-account__container'>
+          <form action='' className='my-account__form'>
+            <div className='my-account__email'>
+              <label htmlFor='email'>Email</label>
+              <div className='my-account__bar'></div>
+              <input
+                type='email'
+                id='email'
+                name='email'
+                placeholder='example@email.com'
+                required
+              />
+            </div>
+            <div className='my-account__password'>
+              <label htmlFor='password'>Password</label>
+              <div className='my-account__bar'></div>
+              <input
+                type='password'
+                id='password'
+                name='password'
+                placeholder='password'
+                pattern='[a-zA-Z0-9.!#$%&’*+/=?^_`{|}~-]+@[a-zA-Z0-9-]+(?:\.[a-zA-Z0-9-]+)*'
+                required
+              />
+            </div>
+            <button type='submit' className='my-account__submit'>
+              Sign Up
+            </button>
+          </form>
+        </div>
+      </div>
     </div>
   );
 };
