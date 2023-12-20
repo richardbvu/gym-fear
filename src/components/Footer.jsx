7a0dd3047777b963d4e5a8e7add8c2e21370f992
Richard <richardvu93@gmail.com>
2023-12-19 17:55:09 -0800

    update

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -11,60 +11,62 @@ function Footer() {
   return (
     <div className='footer'>
       <div className='footer__container'>
-        <div className='footer__socials'>
-          <h1>GYM FEAR</h1>
-          <p>
-            Take your health and mind to the next level with us. Beat your fear
-            and reach your goals.
-          </p>
-          <div className='footer__social-icons'>
-            <a href='/'>
-              <FontAwesomeIcon icon={faTwitter} />
-            </a>
-            <a href='/'>
-              <FontAwesomeIcon icon={faSquareInstagram} />
-            </a>
-            <a href='/'>
-              <FontAwesomeIcon icon={faTiktok} />
-            </a>
-            <a href='/'>
-              <FontAwesomeIcon icon={faYoutube} />
-            </a>
+        <div className='footer__grid'>
+          <div className='footer__socials'>
+            <h1>GYM FEAR</h1>
+            <p>
+              Take your health and mind to the next level with us. Beat your
+              fear and reach your goals.
+            </p>
+            <div className='footer__social-icons'>
+              <a href='/'>
+                <FontAwesomeIcon icon={faTwitter} />
+              </a>
+              <a href='/'>
+                <FontAwesomeIcon icon={faSquareInstagram} />
+              </a>
+              <a href='/'>
+                <FontAwesomeIcon icon={faTiktok} />
+              </a>
+              <a href='/'>
+                <FontAwesomeIcon icon={faYoutube} />
+              </a>
+            </div>
           </div>
-        </div>
-        <div className='footer__links'>
-          <div className='footer__links-title'>About Us</div>
-          <ul className='footer__links-info'>
-            <li>
-              <Link to='/gallery'>Gallery</Link>
-            </li>
-            <li>
-              <Link to='/schedule'>Schedule</Link>
-            </li>
-            <li>
-              <Link to='/pricing'>Membership</Link>
-            </li>
-            <li>
-              <Link to='/classes'>Classes</Link>
-            </li>
-            <li>
-              <Link to='/contact'>Contact</Link>
-            </li>
-          </ul>
-        </div>
-        <div className='footer__hours'>
-          <div className='footer__hours-title'>Working Hours</div>
-          <ul className='footer__hours-info'>
-            <li>
-              <span>Monday - Friday</span>5:00 AM - 11:00 PM
-            </li>
-            <li>
-              <span>Saturday</span>5:00 AM - 10:00 PM
-            </li>
-            <li>
-              <span>Sunday</span>5:00 AM - 9:00 PM
-            </li>
-          </ul>
+          <div className='footer__links'>
+            <div className='footer__links-title'>About Us</div>
+            <ul className='footer__links-info'>
+              <li>
+                <Link to='/gallery'>Gallery</Link>
+              </li>
+              <li>
+                <Link to='/schedule'>Schedule</Link>
+              </li>
+              <li>
+                <Link to='/pricing'>Membership</Link>
+              </li>
+              <li>
+                <Link to='/classes'>Classes</Link>
+              </li>
+              <li>
+                <Link to='/contact'>Contact</Link>
+              </li>
+            </ul>
+          </div>
+          <div className='footer__hours'>
+            <div className='footer__hours-title'>Working Hours</div>
+            <ul className='footer__hours-info'>
+              <li>
+                <span>Monday - Friday</span>5:00 AM - 11:00 PM
+              </li>
+              <li>
+                <span>Saturday</span>5:00 AM - 10:00 PM
+              </li>
+              <li>
+                <span>Sunday</span>5:00 AM - 9:00 PM
+              </li>
+            </ul>
+          </div>
         </div>
       </div>
     </div>
