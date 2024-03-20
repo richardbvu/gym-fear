ac38bd078f7fa69e0f8ef69ada12db0bb860bc1f
Richard <richardvu93@gmail.com>
2024-03-19 19:45:47 -0700

    back end url

--- src/utils/HandleApi.js
+++ src/utils/HandleApi.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 // const baseUrl = "http://localhost:5000";
-const baseUrl = "https://gym-fear-backend.vercel.app/";
+const baseUrl = "https://gym-fear-backend.onrender.com";
 
 const getAllToDo = (setToDo) => {
   axios.get(baseUrl).then(({ data }) => {
